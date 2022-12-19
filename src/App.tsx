a8f25ff38935e7961f6462136cbd93cdc433f66f
youredith <110220308+BornInTheMiddleOfUniverse@users.noreply.github.com>
2022-12-19 15:38:04 +0900

    react-beautiful-dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
+import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd";
+import styled from "styled-components";
+import { toDoState } from "./atoms";
+import {useRecoilState} from "recoil";
 import React from "react";
-import { useRecoilState } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
+import DraggableCard from "./components/DragabbleCard";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  
-  const [hours, setHours] = useRecoilState(hourSelector);
-  const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value); 
-  }
-  const onHoursChange = (event:React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };  
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = ({destination, source}: DropResult) => {};
+
   return (
-    <div>
-      <input
-        value={hours}
-        onChange={onHoursChange}
-        type="number"
-        placeholder="Hours"
-      />
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+      <Boards>
+        <Droppable droppableId="one">
+          {(magic) => 
+            <Board ref={magic.innerRef} {...magic.droppableProps}>
+              {toDos.map((toDo, index) => (
+                <DraggableCard key={toDo} index={index} toDo={toDo} />
+              ))}
+              {magic.placeholder}
+            </Board>
+          }
+        </Droppable>
+      </Boards>
+    </Wrapper>
+    </DragDropContext>
+
+    
   );
 }
 
